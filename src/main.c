71799e9e89eccb6b5624dd9041193fd192db2b76
blurryroots <blurryroots@posteo.de>
2016-06-06 18:55:56 +0200

    changed test calls in main program

--- src/main.c
+++ src/main.c
@@ -6,9 +6,10 @@
 
 int
 main (int argc, char **argv) {
-	cons_t *prog;
-
-	prog = cons (str ("Hans die wurst!"), cons (num (42), cons (boolean (false), NULL)));
+	cons_t *prog =
+		(cons (str ("Hans die wurst!"),
+			   cons (num (42.1618),
+					 cons (boolean (false), NULL))));
 
 	print_cons (prog);
 	printf ("\n");
